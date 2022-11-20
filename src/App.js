5f507656c80a3f7963f2157f49987b1aec66be8a
Matthew Rachid Romie Nascimento <matthewromie@hotmail.com>
2022-11-20 15:16:23 -0300

    Finish Challenge

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { useState } from 'react';
 import './App.css';
 import CardContainer from './components/CardContainer';
 import Question from './components/Question';
-import image3 from './images/bg-pattern-desktop.svg';
-
-import image2 from './images/illustration-box-desktop.svg';
-import image1 from './images/illustration-woman-online-desktop.svg';
+import imageDesktop3 from './images/bg-pattern-desktop.svg';
+import imageMobile3 from './images/bg-pattern-mobile.svg';
+import imageDesktop2 from './images/illustration-box-desktop.svg';
+import imageDesktop1 from './images/illustration-woman-online-desktop.svg';
+import imageMobile1 from './images/illustration-woman-online-mobile.svg';
 
 
 function App() {
@@ -15,11 +16,20 @@ function App() {
   return (
     <div className='container'>
       <div className='container-intern'>
-      <img src={image2} className="image2" />
+      <img src={imageDesktop2} className="image2" />
       <CardContainer>
         <div className='left-container'>
-          <img src={image1} className="image1" />
-          <img src={image3} className="image3" />
+          
+          <picture>
+          <source media="(max-width:867px)" className="image1" srcset={imageMobile1}/>
+           <img src={imageDesktop1} className="image1" />
+          </picture>
+
+          <picture>
+          <source media="(max-width:867px)" className="image3" srcset={imageMobile3}/>
+          <img src={imageDesktop3} className="image3" />
+          </picture>
+
         </div>
         <div className='right-container'>
           <h1>
